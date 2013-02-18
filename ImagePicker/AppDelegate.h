e1726b91ed50c6fb0f7f5018c84a78629c57d57c
Kouichi ABE (WALL) <kouichi@magickworx.com>
2013-02-18 23:04:27 +0900

    Initial Commit

---
+++ ImagePicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImagePicker
+//
+//  Created by Kouichi ABE (WALL) on 2013/02/18.
+//  Copyright (c) 2013年 Kouichi ABE (WALL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImagePicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ImagePicker
+//
+//  Created by Kouichi ABE (WALL) on 2013/02/18.
+//  Copyright (c) 2013年 Kouichi ABE (WALL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
